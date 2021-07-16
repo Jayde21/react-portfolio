5b205ffcf7c718651facd166615927b925539914
Jayde21 <cosmicJayde@gmail.com>
2021-07-16 09:19:52 -0600

    fourth commit

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -4,6 +4,7 @@ import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
     render(){
+        //hi there
         return (
             <div>
                 <h2>Portfolio items go here...</h2>
