717c418dacac9e75f97384053b82152f4ec5b497
Jayde21 <cosmicjayde@gmai.com>
2021-07-21 09:19:17 -0600

    commit13

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -5,24 +5,20 @@ import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component {
     constructor() {
         super();
+
         console.log("Portfolio container had rendered");
-    }
-
-    PortfolioItems(){
-        const data = ["YellowNotFound", "OrangeNotfound", "GreenNotFound"];
-
-        return data.map(item => {
-            return <PortfolioItem title={item} />;
-        })
     }
 
     render(){
         //hi there
         return (
             <div>
-                <h2>Portfolio items go here...</h2>
+                <h2>Portfolio items go here updated...</h2>
 
-                {this.PortfolioItems()}
+                <PortfolioItem/>
+                <PortfolioItem/>
+                <PortfolioItem/>
+                <PortfolioItem/>
             </div>
         )
     }
